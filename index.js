40861c3d7f05f6bc7a37644644a4e0ea4f45c01c
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-06-09 20:28:30 +0600

    jwt middlwere make

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[0];
+  jwt.verify(token, process.env.JSON_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjdcdj5.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -48,8 +65,8 @@ async function run() {
       res.send(result);
     });
     // get all class bookmark
-    app.post("/book-class/:email", async (req, res) => {
-      const query = { email: req.params.email };
+    app.get("/book-class/:email", async (req, res) => {
+      const query = { instructor_email: req.params.email };
       const result = await bookingsCollection.find(query).toArray();
       res.send(result);
     });
